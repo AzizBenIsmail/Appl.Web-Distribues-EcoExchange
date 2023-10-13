c5a54fbf5f2d45e1d8d7bb4c7c46c3cc05cc9422
MelkiAmal <amal.melki@esprit.tn>
2023-10-13 23:01:20 +0100

    Trade microservice initilization

---
+++ MicroserviceTrade/src/main/java/com/trade/microservicetrade/MicroserviceTradeApplication.java
@@ -0,0 +1,13 @@
+package com.trade.microservicetrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTradeApplication.class, args);
+	}
+
+}
