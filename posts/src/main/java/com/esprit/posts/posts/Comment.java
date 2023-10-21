cdcd0a945e6771496b4e3fea17547804a32e3721
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-10-21 12:59:34 +0100

    update anotations

--- posts/src/main/java/com/esprit/posts/posts/Comment.java
+++ posts/src/main/java/com/esprit/posts/posts/Comment.java
@@ -6,10 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Entity
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment implements Serializable{
         @Id
     @GeneratedValue
