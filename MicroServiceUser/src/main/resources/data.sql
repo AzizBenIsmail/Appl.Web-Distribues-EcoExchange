a83aaf7124e122ed4bcef56fd19e6a480ec7029f
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-24 20:13:28 +0100

    refact: adding 10 default users - Resources

--- MicroServiceUser/src/main/resources/data.sql
+++ MicroServiceUser/src/main/resources/data.sql
@@ -1,1 +1,11 @@
-INSERT INTO user (id, username, firstName, lastName, email, age, address, phoneNumber)VALUES(1, 'user1', 'John', 'Doe', 'john.doe@example.com', 30, '123 Main St','555-123-4567');
+INSERT INTO user (id, username, firstname, lastname, email, age, address, phonenumber) VALUES
+                (1, 'user1', 'John', 'Doe', 'john.doe@example.com', 30, '123 Main St', '555-123-4567'),
+                (2, 'user2', 'Jane', 'Smith', 'jane.smith@example.com', 28, '456 Elm St', '555-987-6543'),
+                (3, 'user3', 'Michael', 'Johnson', 'michael.johnson@example.com', 35, '789 Oak St', '555-456-7890'),
+                (4, 'user4', 'Emily', 'Davis', 'emily.davis@example.com', 32, '321 Pine St', '555-789-1234'),
+                (5, 'user5', 'David', 'Wilson', 'david.wilson@example.com', 29, '567 Maple St', '555-234-5678'),
+                (6, 'user6', 'Sophia', 'Anderson', 'sophia.anderson@example.com', 27, '890 Cedar St', '555-876-5432'),
+                (7, 'user7', 'Matthew', 'Brown', 'matthew.brown@example.com', 31, '432 Birch St', '555-345-6789'),
+                (8, 'user8', 'Olivia', 'Taylor', 'olivia.taylor@example.com', 33, '678 Walnut St', '555-654-3210'),
+                (9, 'user9', 'Daniel', 'Thomas', 'daniel.thomas@example.com', 26, '987 Pineapple St', '555-987-6543'),
+                (10, 'user10', 'Emma', 'Robinson', 'emma.robinson@example.com', 30, '543 Cherry St', '555-321-6547');
