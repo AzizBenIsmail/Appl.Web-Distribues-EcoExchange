fdaf7ec8062e09b22881b3d4881a5804ae845c5f
Amen allah Jouini <amenallah.jouini@esprit.tn>
2023-10-24 22:32:10 +0100

    docker image deploy

---
+++ MicroserviceOrganisation/src/main/resources/organisations.sql
@@ -0,0 +1,5 @@
+INSERT INTO ORGANISATION (id, title, description, category) VALUES
+                                                            (1, 'Tunisianet', 'Tunisianet vous offre des variétés solutions techniques', 'Technologie'),
+                                                            (2, 'Orange', 'Orange vous aide à developper vos projets et devenir un entrepreneur', 'FabLab'),
+                                                            (3, 'Aramex', 'Nous vous garantie une livraison rapide et fiable', 'Livraison')
+
