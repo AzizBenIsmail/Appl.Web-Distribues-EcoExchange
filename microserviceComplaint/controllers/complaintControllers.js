82c9025feace77ff44579d1f3d2c0618d6bc6a77
Elyes Boudhina <elyes.boudhina@etudiant-fsegt.utm.tn>
2023-10-25 20:22:04 +0100

    style: changing style - ComplaintMS

--- microserviceComplaint/controllers/complaintControllers.js
+++ microserviceComplaint/controllers/complaintControllers.js
@@ -1,62 +1,63 @@
-const complaintModel=require("../Models/complaint"); 
-const addcomplaint=async(req,res,next)=>{ 
+const complaintModel = require("../Models/complaint");
+
+const addComplaint = async (req, res, next) => {
     try {
-        const {object,content}=req.body;
-        console.log(req.body);  
-        const complaint=new complaintModel({object,content});
+        const { object, content } = req.body;
+        console.log(req.body);
+        const complaint = new complaintModel({ object, content });
         const addedcomplaint = await complaint.save();
         res.status(200).json(addedcomplaint);
     } catch (error) {
-        res.status(500).json({message:error.message});
+        res.status(500).json({ message: error.message });
     }
 };
-const getcomplaints=async(req,res,next)=>{
+const getComplaints = async (req, res, next) => {
     try {
         const complaints = await complaintModel.find();
-        if(!complaints||complaints.length===0){
-            throw new Error('complaints not found !')
+        if (!complaints || complaints.length === 0) {
+            throw new Error("complaints not found !");
         }
-        res.status(200).json({complaints});
+        res.status(200).json({ complaints });
     } catch (error) {
-        res.status(500).json({message:error.message});
+        res.status(500).json({ message: error.message });
     }
-    
 };
-const updatecomplaint=async(req,res,next)=>{
+const updateComplaint = async (req, res, next) => {
     try {
-        const {id} = req.params;
-        const {object,content}=req.body;
-        const checkIfcomplaintExists=await complaintModel.findById(id);
-        if(!checkIfcomplaintExists){
+        const { id } = req.params;
+        const { object, content } = req.body;
+        const checkIfcomplaintExists = await complaintModel.findById(id);
+        if (!checkIfcomplaintExists) {
             throw new Error("complaint not found !");
         }
-        updated=await complaintModel.findByIdAndUpdate(
-            id,{
-                $set:{object,content}
-            },{new:true}
+        updated = await complaintModel.findByIdAndUpdate(
+            id,
+            {
+                $set: { object, content },
+            },
+            { new: true },
         );
-        res.status(200).json({updated});
+        res.status(200).json({ updated });
     } catch (error) {
-        res.status(500).json({message:error.message});
+        res.status(500).json({ message: error.message });
     }
 };
-const deletecomplaint=async(req,res,next)=>{
+const deleteComplaint = async (req, res, next) => {
     try {
-        const {id} = req.params;
-        const checkIfcomplaintExists=await complaintModel.findById(id);
-        if(!checkIfcomplaintExists){
-            throw new Error("complaint not found !");
+        const { id } = req.params;
+        const checkIfcomplaintExists = await complaintModel.findById(id);
+        if (!checkIfcomplaintExists) {
+            throw new Error("Complaint not found !");
         }
         await complaintModel.findByIdAndDelete(id);
-        res.status(200).json("deleted");
-    
+        res.status(200).json("Deleted");
     } catch (error) {
-        res.status(500).json({message:error.message});
+        res.status(500).json({ message: error.message });
     }
 };
-module.exports={
-    addcomplaint,
-    getcomplaints,
-    deletecomplaint,
-    updatecomplaint
-}
+module.exports = {
+    addComplaint,
+    getComplaints,
+    deleteComplaint,
+    updateComplaint,
+};
