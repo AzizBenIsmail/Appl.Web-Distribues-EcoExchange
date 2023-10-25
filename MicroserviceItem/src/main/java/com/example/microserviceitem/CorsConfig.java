95e3586481a94b36cf965478c60ffda769b2741b
AzizBenIsmail <61393700+AzizBenIsmail@users.noreply.github.com>
2023-10-25 01:25:59 +0100

    réglage problème cros

--- MicroserviceItem/src/main/java/com/example/microserviceitem/CorsConfig.java
+++ MicroserviceItem/src/main/java/com/example/microserviceitem/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8055")  // Remplacez par l'URL de votre application Angular
+                .allowedOrigins("http://localhost:4200")  // Remplacez par l'URL de votre application Angular
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
